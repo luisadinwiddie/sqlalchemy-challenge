cdcccf949904d0e036d96b73831b776f162b437c
luisadinwiddie <142279224+luisadinwiddie@users.noreply.github.com>
2023-11-15 22:50:33 -0600

    Add files via upload
    Hi! This is my challenge =)

---
+++ SurfsUp/luisa.app.py
@@ -0,0 +1,41 @@
+# Import the dependencies.
+
+import datetime as dt
+import numpy as np
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
